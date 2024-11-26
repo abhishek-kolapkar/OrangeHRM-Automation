6e8791d5f77cceb3d612db70c412d4c739d7036a
abhishek-kolapkar <abhisheknkolapkar@gmail.com>
2024-11-26 21:43:13 +0530

    config updated

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://opensource-demo.orangehrmlive.com/",
     specPattern: "integration/**/*.cy.{js,jsx,ts,tsx}",
-    include: ["./node_modules/cypress", "cypress/**/*.js"],
+    include: ["./node_modules/cypress", "integration/**/*.js"],
     testIsolation: false,
 
     setupNodeEvents(on, config) {
