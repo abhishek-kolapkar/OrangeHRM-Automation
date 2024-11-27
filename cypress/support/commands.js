8d79bc1ec8c3e1a06188fd0fb13eba6e3d730bbc
abhishek-kolapkar <abhisheknkolapkar@gmail.com>
2024-11-27 14:22:16 +0530

    updated custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,18 @@
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
+Cypress.Commands.add("login", (username = "Admin", password = "admin123") => {
+  cy.visit("/");
+
+  cy.get("input[name='username']").clear().type(username);
+  cy.get("input[name='password']").clear().type(password);
+
+  cy.get(".orangehrm-login-button").click();
+
+  cy.url().should("include", "dashboard");
+});
+
+Cypress.Commands.add("logout", () => {
+  cy.get(".oxd-userdropdown-tab").click();
+  cy.contains("[role=menuitem]", "Logout").click();
+
+  cy.url().should("include", "auth/login");
+});
